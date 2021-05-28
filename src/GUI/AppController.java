3ffd0f55aebb06750c02d40312f9cb1f154c7b94
AyoubFrihaoui <a.frihaoui@esi-sba.dz>
2021-05-28 19:23:01 +0100

    fxml ids added

--- src/GUI/AppController.java
+++ src/GUI/AppController.java
@@ -1,4 +1,34 @@
 package GUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+
 public class AppController {
+    @FXML
+    private AnchorPane dashpane;
+    @FXML
+    private VBox dashvbox;
+    @FXML
+    private HBox hbhome;
+    @FXML
+    private HBox hbwaitroom;
+    @FXML
+    private Label labwaitroom;
+    @FXML
+    private HBox hbpatients;
+    @FXML
+    private Label labpatients;
+    @FXML
+    private HBox hbconst;
+    @FXML
+    private Label labconst;
+    @FXML
+    private HBox hbaccounting;
+    @FXML
+    private Label labaccounting;
+    @FXML
+    private HBox hbappoit;
+    @FXML
+    private Label labappoit;
+
 }
